0b57e0f4490c06df504e0a24d43bbfdc7435866b
Kin Basco <freepeace13@gmail.com>
2018-12-09 16:22:02 +0800

    The configuration file

---
+++ config/laraversine.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+  /*
+  |--------------------------------------------------------------------------
+  | Search measurement unit
+  |--------------------------------------------------------------------------
+  |
+  | This option controls the default search measurement unit
+  |
+  | Supported: "km", "mile"
+  |
+  */
+  'search_by' => 'km'
+];
